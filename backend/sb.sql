e224db328c04a1b814e52360cb61fa6e9c6301b7
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2025-03-03 20:05:46 -0300

    Cadastro de usuário e veículo conluído e login de usuário concluído.

--- backend/sb.sql
+++ backend/sb.sql
@@ -7,15 +7,26 @@ create table owner (
     name varchar(265) not null,
     email varchar(45) not null,
     password varchar(45) not null
-)
+);
 
 create table parking_space (
-    id int primary key
-)
+    id varchar(45) primary key
+);
+
+drop table parking_space;
+
+drop table cars;
 
 create table cars (
     id int primary key auto_increment,
     vehicle_name varchar(265) not null,
     license_plate varchar(45) unique not null,
-    foreign key (id_owner) references owner(id)
-)
+    parking_space varchar(45) unique not null,
+    foreign key (parking_space) references parking_space(id)
+);
+
+insert into parking_space (id) values (101), (102), (103), (104), (105);
+
+select * from parking_space;
+
+select * from cars
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,16 +8,83 @@ app.use(express.json());
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Server rodando na porta ${port}`))
+app.get('/', (req, res) => {
+    return res.send('Hello, World!')
+})
 
-app.post('/cadastro', (req, res) => {
+app.post('/usuario/cadastro', (req, res) => {
     const { name, email, password } = req.body;
-
+    
     const query = 'INSERT INTO owner (name, email, password) VALUES (?, ?, ?)'
     connection.query(query, [name, email, password], (err, results) => {
         if (err) {
-            return res.status(500).json({ success: false, message: 'Erro no servidor'})
+            return res.status(500).json({ success: false, err, message: 'Erro no servidor'})
         }
-        res.status(201).json({success: true, message: 'Sucesso no cadastro!'})
+        res.status(201).json({success: true, results, message: 'Sucesso no cadastro!'})
     })
-}) 
+})
+
+app.post('/usuario/login', (req, res) => {
+    const {name, password} = req.body
+
+    const query = 'SELECT * FROM owner WHERE name = ? AND password = ?'
+    connection.query(query, [name, password], (err, results) => {
+        if (err) {
+            return res.status(500).json({ success: false, message: 'Erro no servidor!'})
+    }
+
+    if (results.length > 0) {
+        res.json({ success: true, message: 'Sucesso no login!' })
+    } else {
+        res.json({ succes: false, message: 'Usuário ou senha incorretos!'})
+    } 
+    })
+})
+
+app.post('/carro/cadastro', (req, res) => {
+    const {vehicle_name, license_plate, parking_space} = req.body
+
+    const query = 'INSERT INTO cars (vehicle_name, license_plate, parking_space) VALUES (?, ?, ?)'
+    connection.query(query, [vehicle_name, license_plate, parking_space], (err, results) => {
+        if(err) {
+            return res.status(500).json({success: false, err, message: 'Erro no Servidor'})
+        }
+        res.status(201).json({success: true, results, message: 'Sucesso no cadastro do veículo'})
+    })
+})
+
+app.get('/carros', (req, res) => {
+
+    const query = 'SELECT * FROM cars'
+    connection.query(query, (err, results) => {
+        if(err) {
+            return res.status(500).json({ success: false, err, message: 'Erro ao buscar carro'})
+        } 
+        res.json({success: true, cars: results})
+    })
+})
+
+app.put('/carros/:id', (req, res) => {
+    const { id } = req.params
+    const { vehicle_name, license_plate } = req.body
+    const query = 'UPDATE cars SET vehicle_name = ?, license_plate = ? WHERE id = ?'
+    connection.query(query, [vehicle_name, license_plate, id], (err) => {
+        if (err) {
+            return res.status(500).json({ success: false, err, message: 'Erro ao editar veículo'})
+        }
+        res.json({ success: true, message: 'Carro editado com sucesso!' })
+    })
+})
+
+app.delete('/carros/:id', (req, res) => {
+    const { id } = req.params
+    const query = 'DELETE FROM cars WHERE id = ?'
+    connection.query(query, [id], (err) => {
+        if (err) {
+            return res.status(500).json({ success: false, err, message: 'Erro ao deletar veículo' })
+        }
+        res.json({ success: true, message: 'Veículo deletado com sucesso' })
+    })
+})
+
+app.listen(port, () => console.log(`Server rodando na porta ${port}`))
